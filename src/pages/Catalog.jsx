d82e186e0ccdb5cd0f496911b744f16492e9bbce
Kesbie <kesbie.nguyen@gmail.com>
2022-09-05 17:32:27 +0700

    push

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import { useParams } from 'react-router';
+
+import PageHeader from '../components/page-header/PageHeader'
+
+import MovieGrid  from '../components/movie-grid/MovieGrid';
+
+import { category as cate } from '../api/tmdbApi';
 
 const Catalog = () => {
+
+  const { category } = useParams();
+
+  console.log(category);
+
   return (
-    <div>Catalog</div>
+    <>
+      <PageHeader>
+        {category === cate.movie ? 'Movie' : 'TV Series'}
+      </PageHeader>
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category}/>
+
+        </div>
+      </div>
+    </>
   )
 }
 
-export default Catalog
+export default Catalog;
